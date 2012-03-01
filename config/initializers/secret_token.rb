221c9758c56a0dd54accc0528ffe54c78b991e87
Neil Panjwani <paniwani@gmail.com>
2012-03-01 15:02:41 -0500

    initial commit. pre bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UiTest2::Application.config.secret_token = '9125707af3ac43daf33a3f54e6eb3dc542216a1426872b1561f604ccca665587fcfb25e318dccf77d39dcf985859ca82bc12bf90381da4e5098b8265ef37017d'
